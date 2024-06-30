56dfa8fc884476b03f0c9c1b0ebd2e14ed9e48ac
Vladyslav <vladprodan6@gmail.com>
2024-06-30 21:11:16 +0200

    fix path button

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,6 +1,6 @@
-import { Button } from '@repo/ui/button';
-import Image from 'next/image';
-import styles from './page.module.css';
+import { Button } from "@repo/ui/components";
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
